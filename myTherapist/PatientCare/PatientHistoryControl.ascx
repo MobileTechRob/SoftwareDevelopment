36904eb396456854d91fda532feef425774f74c0
MobileTechRob <rob.hermann@hotmail.com>
2017-12-30 19:58:25 -0600

    Patient History changes

--- myTherapist/PatientCare/PatientHistoryControl.ascx
+++ myTherapist/PatientCare/PatientHistoryControl.ascx
@@ -1,8 +1,9 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PatientHistoryControl.ascx.cs" Inherits="PatientCare_PatientHistoryControl" %>
 <link id="stylesheetlink" runat="server" href="../stylesheets/sitestyles.css" rel="stylesheet" />
 <h2>Patient History</h2>
-
 <asp:Label runat="server" ID="lblPatientName" Text="" />
-
 <br />
 <br />
+<asp:RadioButtonList runat="server" ID="rblDisplaySort"><asp:ListItem Value="Ascending"></asp:ListItem><asp:ListItem Value="Descending"></asp:ListItem></asp:RadioButtonList>
+<br />
+<asp:Table runat="server" ID="patientHistoryGrid" />
--- myTherapist/PatientCare/PatientHistoryControl.ascx.cs
+++ myTherapist/PatientCare/PatientHistoryControl.ascx.cs
@@ -9,6 +9,8 @@ public partial class PatientCare_PatientHistoryControl : System.Web.UI.UserContr
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        System.Data.DataTable patientHistory = null;
+
         if (Session["PatientName"] != null)
             lblPatientName.Text = Session["PatientName"].ToString();
 
@@ -20,14 +22,12 @@ public partial class PatientCare_PatientHistoryControl : System.Web.UI.UserContr
 
             pager.AddColumn("ApptDate", "", MyDataTypes.DATETIME, true, 35);
             pager.AddColumn("PatientId", "", MyDataTypes.INTEGER, true, 35);
-
             pager.AddColumn("RLU", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("SP", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("KD1", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("LHT", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("LV", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("KD2", "", MyDataTypes.STRING, false, 0);
-
             pager.AddColumn("TherapyPerformed", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("OilsUsed", "", MyDataTypes.STRING, false, 0);
             pager.AddColumn("SessionGoals", "", MyDataTypes.STRING, false, 0);
@@ -38,7 +38,81 @@ public partial class PatientCare_PatientHistoryControl : System.Web.UI.UserContr
             pager.NumberRowsToDisplay = 0;
             pager.WhereClause = String.Format("PatientId = {0}", Session["PatientId"].ToString());
             pager.Sort = MyDataSort.DESC;
-            pager.BuildTable();
+
+            patientHistory = pager.BuildTable();
+            int rowIndex = 0;
+
+            TableRow row = null;
+            TableCell columnHeader = null;
+            TableCell pulseStrength = null;
+            System.Data.DataRow tableRow = null;
+            int itemArrayIndex = 0;
+
+            row = new TableRow();
+            columnHeader = new TableCell();
+            columnHeader.Text = "ApptDate";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "RLU";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "SP";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "KD";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "LHT";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "LV";
+            row.Cells.Add(columnHeader);
+
+            columnHeader = new TableCell();
+            columnHeader.Text = "KD";
+            row.Cells.Add(columnHeader);
+            
+            patientHistoryGrid.Rows.Add(row);
+            row = new TableRow();
+            row.Height = 20;
+
+            // blank row
+            patientHistoryGrid.Rows.Add(row);
+            
+
+            while (rowIndex < patientHistory.Rows.Count)
+            {
+                tableRow = patientHistory.Rows[rowIndex];                
+                row = new TableRow();
+                itemArrayIndex = 0;
+
+                while (itemArrayIndex < 7)
+                {
+                    pulseStrength = new TableCell();
+                    pulseStrength.Text = tableRow.ItemArray[itemArrayIndex].ToString();
+                    row.Cells.Add(pulseStrength);
+
+                    if (itemArrayIndex == 0)
+                        itemArrayIndex += 2;
+                    else
+                        itemArrayIndex += 1;
+
+                    itemArrayIndex = itemArrayIndex + 1;
+                }
+                
+                patientHistoryGrid.Rows.Add(row);
+                row = new TableRow();
+
+                // blank row
+                patientHistoryGrid.Rows.Add(row);
+
+                rowIndex++;
+            }                       
         }
     }
 }
